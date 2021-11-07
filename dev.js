6b0a8f5787c7a4bc3c5570370f51274acf8ab013
PotatoHD404 <31506993+PotatoHD404@users.noreply.github.com>
2021-11-07 23:38:13 +0300

    Added json write and read

--- dev.js
+++ dev.js
@@ -1,10 +1,14 @@
 import {genJoinUrl, getCredFromUrl} from "./scripts/utils.js";
 import {config} from "dotenv";
+import {writeFileSync, readFileSync} from "fs"
 
 config()
-
+// https://github.com/electron/electron/blob/main/docs/api/app.md#appgetpathname
 // console.log(process.env.ZOOM_LINK)
 // console.log(...Object.values(getCredFromUrl(process.env.ZOOM_LINK)))
-console.log(genJoinUrl(...Object.values(getCredFromUrl(process.env.ZOOM_LINK))))
-
-
+// console.log(genJoinUrl(...Object.values(getCredFromUrl(process.env.ZOOM_LINK))))
+// console.log(JSON.stringify(getCredFromUrl(process.env.ZOOM_LINK)))
+// writeFileSync('userdata.json', JSON.stringify(getCredFromUrl(process.env.ZOOM_LINK)))
+// const data = readFileSync('userdata.json', 'utf-8')
+// const userdata = JSON.parse(data);
+// console.log(userdata)
